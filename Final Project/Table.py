4e26c6945dc39994cf56e9456479bfa513431ca7
nhanpham-ptn <nhanpham.ptn2110@gmail.com>
2024-11-30 04:48:54 +0700

    Update Table.py

--- Final Project/Table.py
+++ Final Project/Table.py
@@ -24,24 +24,25 @@ class Board():
         self.grid = create_board()
         self.window = window 
         self.selected_cell = None
+        self.attempt = 3
     def draw_board(self):
         def create_cells():
             board = []
             for row in range(9):
-                row = []
+                rows = []
                 for col in range(9):
-                    cell = pygame.Rect(col * 50 + 6, row * 50 +6, 50, 50)
-                    row.append(cell)
-                board.append(row)
+                    cell = pygame.Rect(col * (510/9), row * (510/9), (510/9), (510/9))
+                    rows.append(cell)
+                board.append(rows)
             return board 
-        def draw_tiles():
+        def draw_lines():
             for i in range(10):
                 if i % 3 ==0:
                     thickness = 6
                 else:
                     thickness = 3
-                pygame.draw.line(window, Black, (0, i * 50), (510, i * 50), thickness)
-                pygame.draw.line(window, Black, (i * 50, 0), (i * 50, 510), thickness)
+                pygame.draw.line(window, Black, (0, i * (510/9)), (510, i * (510/9)), thickness)
+                pygame.draw.line(window, Black, (i * (510/9), 0), (i * (510/9), 510), thickness)
         def draw_cells(cells):
                 for row in range(9):
                     for col in range(9):
@@ -52,7 +53,7 @@ class Board():
 
         cells = create_cells()
         self.window.fill(white)  
-        draw_tiles()
+        draw_lines()
         draw_cells(cells)
         if self.selected_cell:
                 (row, col) = self.selected_cell
@@ -60,13 +61,97 @@ class Board():
         
     def select(self, pos):
         (x, y) = pos
-        col = int(x // 50)  
-        row = int(y // 50)
+        col = int(x // (510/9))  
+        row = int(y // (510/9))
         if 0 <= row <= 9 and 0 <= col <= 9:
             self.selected_cell = (row, col)
-    def adding_number(self):
-        
+        return self.selected_cell 
+    def adding_number(self, num):
+        (row, col) = self.selected_cell
+        if not self.grid[row][col] == 0:
+            pass
+        else:
+            self.grid[row][col] = num
+            return True
+        return False
+    def deselect(self):
+        self.selected_cell = None
+    
+    def correct(self, num):
+        if self.adding_number(num):
+            (row, col) = self.selected_cell
+            cell = pygame.Rect(col * (510/9), row * (510/9), (510/9), (510/9))
+            if checking(self.grid, num , row, col):
+                pygame.draw.rect(self.window, green, cell, 5)
+                self.grid[row][col] = num
+                pygame.display.update()
+                pygame.time.delay(1000)
+                return True
+            else:
+                pygame.draw.rect(self.window, red, cell, 5)
+                self.grid[row][col] = 0 
+                pygame.display.update()
+                pygame.time.delay(1000)
+                return False
 
+
+    def get_attempt(self):
+        return self.attempt
+
+
+
+def play_game():
+        grid = Board(window)
+        selected_cell = None
+        run = True
+
+
+        while run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    run = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    grid.draw_board()
+                    if event.button == 1:
+                        pos = pygame.mouse.get_pos()
+                        if pos[0] < 510 and pos[1] < 510:
+                            selected_cell = grid.select(pos)
+                            (row, col) = selected_cell
+                elif event.type == pygame.KEYDOWN and selected_cell:
+                    if event.key == pygame.K_1:
+                         grid.correct(1)
+                    elif event.key == pygame.K_2:
+                         grid.correct(2)
+                    elif event.key == pygame.K_3:
+                         grid.correct(3)
+                    elif event.key == pygame.K_4:
+                         grid.correct(4)
+                    elif event.key == pygame.K_5:
+                         grid.correct(5)
+                    elif event.key == pygame.K_6:
+                         grid.correct(6)
+                    elif event.key == pygame.K_7:
+                         grid.correct(7)
+                    elif event.key == pygame.K_8:
+                         grid.correct(8)
+                    elif event.key == pygame.K_9:
+                         grid.correct(9)
+                    elif event.key in [pygame.K_BACKSPACE, pygame.K_DELETE]:  
+                         grid.grid[row][col] = 0
+                    elif event.key == pygame.K_ESCAPE:  
+                         grid.deselect()
+                         selected_cell = None
+            grid.draw_board()
+            pygame.display.update()
+        pygame.quit()
+
+    
+
+play_game()
+
+
+
+        
 
 
         
