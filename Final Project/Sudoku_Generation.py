0ee0e7a863d64cc84b2145ae14ed7e4c2a290b8b
nhanpham-ptn <nhanpham.ptn2110@gmail.com>
2024-12-09 03:29:36 +0700

    Update Sudoku_Generation.py

--- Final Project/Sudoku_Generation.py
+++ Final Project/Sudoku_Generation.py
@@ -4,20 +4,20 @@ import random
 #create a board
 def create_board():
     board = []
+    #make an entire board filled of 0s first
     for i in  range(9):
         spaces = []
         for j in range(9):
             spaces.append(0)
         board.append(spaces)
-
+    #fill the board with apropriate numbers in diagonal 3x3 squares
     for i in range(0, 9, 3):
         nums = [1,2,3,4,5,6,7,8,9]
         random.shuffle(nums)
         for row in range(3):
             for column in range(3):
                 board[i + row][i + column] = nums.pop()
-    
-
+    # write a function for checking if the number filled is relevant
     def is_num_safe(board, row, column, num):
         if num in board[row]:
             return False
@@ -33,7 +33,7 @@ def create_board():
                 if board[start_row + i][start_column + j] == num:
                     return False
         return True
-
+    # write a function filling numbers that have been checked by the function above in all empty cells  
     def fill_cells(board, row, column):
         if row == 9:
             return True
@@ -43,7 +43,7 @@ def create_board():
         if board[row][column] != 0:
             return fill_cells(board, row, column + 1)
         
-
+    # filling the number from 1 to 9 
         for num in range(1, 10):
             if is_num_safe(board, row, column, num):
                 board[row][column] = num
@@ -53,6 +53,9 @@ def create_board():
         return False
     
     fill_cells(board, 0, 0)
-
     return board
+
+
+
+
 
